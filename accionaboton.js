8e83d6d26b0b67b676c5edaa835c2268e1b265a8
vegas29 <39704797+vegas29@users.noreply.github.com>
2020-06-02 22:03:26 -0500

    Primer subida
    Componentes básicos
    Se sube los archivos html, css y js

---
+++ accionaboton.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  $(document).ready(function () {
+
+    $(".no").hover(function () {
+      var v = getRandomInt(1, $(window).height() - 100);
+      var h = getRandomInt(1, $(window).width() - 100);
+      $(this).css({ top: v + "px", left: h + "px" });
+    });
+
+    $(".si").click(function () {
+        alert("Sabia que dirias que si");
+    });
+
+    
+  });
